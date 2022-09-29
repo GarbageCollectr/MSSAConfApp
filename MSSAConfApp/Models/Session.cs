e3bda450bce112870bb7e6a84b4c7058e60805fb
Alex <alexwerner569@hotmail.com>
2022-09-29 09:58:15 -0700

    added old code

---
+++ MSSAConfApp/Models/Session.cs
@@ -0,0 +1,26 @@
+﻿using System.Text.Json;
+
+namespace MSSAConfApp.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Location { get; set; }
+        public string KeySpeaker { get; set; }
+        public string Track1 { get; set; }
+        public string Track2 { get; set; }
+        public int Seats { get; set; }
+        //public List<Speakers>[] Speakers { get; set; }
+
+
+
+        public override string ToString()
+        {
+           return JsonSerializer.Serialize<Session>(this);
+        }
+    }
+
+    
+}
---
+++ MSSAConfApp/Models/SessionRepo.cs
@@ -0,0 +1,18 @@
+﻿namespace MSSAConfApp.Models
+{
+    public class SessionRepo
+    {
+        private List<Session> sessions = new List<Session>();
+        
+
+        public List<Session> GetSessions()
+        {
+            return sessions;
+        }
+
+        public Session GetSession(int id)
+        {
+            return sessions.FirstOrDefault(s => s.Id == id);
+        }
+    }
+}
