a09f27f5cf1be7d74af90bce14a5726d9e9f0888
Alex <alexwerner569@hotmail.com>
2022-09-29 09:48:58 -0700

    initial

---
+++ MSSAConfApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MSSAConfApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
