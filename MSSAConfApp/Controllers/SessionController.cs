3c51c6c9e0bcb152a8000ba235a239d11e349f0f
Alex <alexwerner569@hotmail.com>
2022-09-29 13:35:07 -0700

    again

---
+++ MSSAConfApp/Controllers/SessionController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MSSAConfApp.Data;
+
+namespace MSSAConfApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        private SessionsContext _sessionsContext;
+        public IEnumerable<Session> MySessions { get; private set; }
+        public SessionController(SessionsContext context)
+        {
+            _sessionsContext = context;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            MySessions = _sessionsContext.NewTables;
+            
+            return MySessions.ToString();
+        }
+    }
+}
